00ce66a77c18f559aec56ab7956a7a0f1c8c63b4
jorge <j0rg3e93@gmail.com>
2024-01-19 23:14:29 +0100

    CREATE: Se crea los archivos necesarios para los todos

---
+++ entities/todos/model.ts
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+
+const todoSchema = new mongoose.Schema({
+  usuario: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Usuario',
+    required: true
+  },
+  tarea: {
+    type: String,
+    required: true
+  },
+  completado: {
+    type: Boolean,
+    default: false
+  },
+  eliminado: {
+    type: Boolean,
+    default: false
+  }
+}, {
+  timestamps: true
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+export default Todo;
