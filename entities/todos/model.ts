f8e066307f60320a2d7ec63f32969f9a9e487e53
jorge <j0rg3e93@gmail.com>
2024-01-19 23:15:48 +0100

    UPDATE: Se actualiza una configuración del schema

--- entities/todos/model.ts
+++ entities/todos/model.ts
@@ -1,27 +1,31 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const todoSchema = new mongoose.Schema({
-  usuario: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Usuario',
-    required: true
+const todoSchema = new mongoose.Schema(
+  {
+    usuario: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Usuario",
+      required: true,
+    },
+    tarea: {
+      type: String,
+      required: true,
+    },
+    completado: {
+      type: Boolean,
+      default: false,
+    },
+    eliminado: {
+      type: Boolean,
+      default: false,
+    },
   },
-  tarea: {
-    type: String,
-    required: true
-  },
-  completado: {
-    type: Boolean,
-    default: false
-  },
-  eliminado: {
-    type: Boolean,
-    default: false
+  {
+    versionKey: false,
+    timestamps: true,
   }
-}, {
-  timestamps: true
-});
+);
 
-const Todo = mongoose.model('Todo', todoSchema);
+const Todo = mongoose.model("Todo", todoSchema);
 
 export default Todo;
