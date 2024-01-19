ca02e8126b16fa4371fc73abc432230aa0ee424a
jorge <j0rg3e93@gmail.com>
2024-01-19 22:54:36 +0100

    CREATE: Se crea la estructura base del proyecto

---
+++ app.ts
@@ -0,0 +1,20 @@
+import cors from "cors";
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+// ROUTER
+// import godsRouter from "./entities/gods/router";
+// import usersRouter from "./entities/users/router";
+
+app.use(cors());
+app.get("/", (Request, Response) => {
+  Response.send("Healcheck: ok");
+});
+// app.use("/gods/", godsRouter);
+// app.use("/user/", usersRouter);
+// app.use(errorHandler);
+
+app.listen(3000, () => console.log("Servidor levantado en 3000"));
