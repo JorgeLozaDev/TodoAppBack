044d1754f1a806c79f1cd8b10ffc36d05f7b5f71
Jorge <j0rg3e93@gmail.com>
2024-06-13 11:32:46 +0200

    UPDATE: Se agrega el endpoint para el perfil del usuario

--- entities/users/router.ts
+++ entities/users/router.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { login, signIn } from "./controller";
+import { getProfile, login, signIn } from "./controller";
+import { authMiddleware } from "../../middlewares/authMIddleware";
 
 const router = express.Router();
 
 router.post("/addUser", signIn);
 router.post("/login", login);
+router.get("/profile", authMiddleware, getProfile);
 
 export default router;
