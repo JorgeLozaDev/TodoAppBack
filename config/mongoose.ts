727ce89d0f8149068568994bf99642b09b090815
jorge <j0rg3e93@gmail.com>
2024-07-09 00:06:46 +0200

    ADD: Agregamos funcionalidad, la cual agrega un usuario admin, si este no existe en la base de datos

--- config/mongoose.ts
+++ config/mongoose.ts
@@ -1,10 +1,30 @@
 import mongoose from "mongoose";
 import CONF from "../core/config";
+import User from "../entities/users/model";
+import bcrypt from "bcrypt";
 
 mongoose
   .connect(CONF.DDBB_URL + CONF.DDBB_NAME, {})
 
-  .then(() => console.log("Conectado correctamente a la BBDD"))
+  .then(async () => {
+    console.log("Conectado correctamente a la BBDD");
+    const adminExist = await User.findOne({ role: "admin" });
+    if (!adminExist) {
+      const hashedPassword = await bcrypt.hash(
+        CONF.ADMIN_PASSWORD,
+        CONF.BCRYTP_LOOP
+      );
+      const adminUser = new User({
+        nombre: CONF.ADMIN_USERNAME,
+        email: CONF.ADMIN_EMAIL,
+        password: hashedPassword,
+        role: "admin",
+      });
+
+      await adminUser.save();
+      console.log("Usuario admin creado");
+    }
+  }) 
   .catch((e) => console.log("error" + e));
 
 export = mongoose;
