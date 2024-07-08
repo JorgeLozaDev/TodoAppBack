815cd80e0ea1c2b5233f7869f979c95254883fac
jorge <j0rg3e93@gmail.com>
2024-07-09 00:07:40 +0200

    ADD: Se agregan variables de entorno para crear el usuario de admin

--- core/config.ts
+++ core/config.ts
@@ -1,9 +1,11 @@
 const CONF = {
-    DDBB_URL: process.env.DDBB_URL!,
-    DDBB_NAME: process.env.DDBB_NAME!,
-    BCRYTP_LOOP: parseInt(process.env.BCRYTP_LOOP!, 10),
-    JWT_SECRET: process.env.JWT_SECRET!,
-  };
-  
-  export = CONF;
-  
+  DDBB_URL: process.env.DDBB_URL!,
+  DDBB_NAME: process.env.DDBB_NAME!,
+  BCRYTP_LOOP: parseInt(process.env.BCRYTP_LOOP!, 10),
+  JWT_SECRET: process.env.JWT_SECRET!,
+  ADMIN_USERNAME: process.env.ADMIN_USERNAME,
+  ADMIN_EMAIL: process.env.ADMIN_EMAIL,
+  ADMIN_PASSWORD: process.env.ADMIN_PASSWORD,
+};
+
+export = CONF;
