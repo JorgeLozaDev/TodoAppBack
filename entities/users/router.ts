c331345928aaacb8307cc36251561cc158adfdba
jorge <j0rg3e93@gmail.com>
2024-07-31 00:20:07 +0200

    UPDATE: Se añade endpoint para actualizar los datos del usuario

--- entities/users/router.ts
+++ entities/users/router.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import { getProfile, login, signIn } from "./controller";
+import { getProfile, login, signIn, updateProfile } from "./controller";
 import { authMiddleware } from "../../middlewares/authMIddleware";
 
 const router = express.Router();
 
 router.post("/addUser", signIn);
 router.post("/login", login);
-router.get("/profile", authMiddleware, getProfile); 
+router.get("/profile", authMiddleware, getProfile);
+router.put("/updateprofile", authMiddleware, updateProfile);
 
 export default router;
