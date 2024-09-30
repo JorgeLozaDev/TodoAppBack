11af045dab6bca60e349573bafe68b81cf665c42
jorge <j0rg3e93@gmail.com>
2024-10-01 00:03:32 +0200

    UPDATE: Se prepara el endpoint addtodo, para que reciba los parametros

--- entities/todos/controller.ts
+++ entities/todos/controller.ts
@@ -2,24 +2,28 @@ import express, { NextFunction, Request, Response } from "express";
 import AuthenticatedRequest from "../../core/customInterfaces";
 
 export const addTodo = async (
-    req: AuthenticatedRequest,
-    res: Response,
-    next: NextFunction
-  ) => {
-    try {
-      const userId = req.user.id; // Obtén el ID del usuario autenticado desde el middleware
-  
-      // Busca al usuario por ID
-      // const user = await User.findById(userId);
-  
-      // if (!user) {
-      //   const error = new Error("Usuario no encontrado");
-      //   (error as any).status = 404;
-      //   throw error;
-      // }
-  
-      // res.status(200).json(user);
-    } catch (error) {
-      next(error);
-    }
-  };
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const userId = req.user.id; // Obtén el ID del usuario autenticado desde el middleware
+    // Obtener datos del cuerpo de la solicitud
+    const { tituloTarea, descripcion, tiempo, tiempoFin, estado, prioridad } =
+      req.body;
+    console.log(tituloTarea, descripcion, tiempo, tiempoFin, estado, prioridad);
+
+    // Busca al usuario por ID
+    // const user = await User.findById(userId);
+
+    // if (!user) {
+    //   const error = new Error("Usuario no encontrado");
+    //   (error as any).status = 404;
+    //   throw error;
+    // }
+
+    // res.status(200).json(user);
+  } catch (error) {
+    next(error);
+  }
+};
