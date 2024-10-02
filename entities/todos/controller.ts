4b3a47179293a353db3b07ae50c27a8351a0b680
jorge <j0rg3e93@gmail.com>
2024-10-02 23:59:32 +0200

    UPDATE: Se actualiza el controlador de addtodos, para que guarde la tarea correctamente

--- entities/todos/controller.ts
+++ entities/todos/controller.ts
@@ -1,5 +1,13 @@
 import express, { NextFunction, Request, Response } from "express";
 import AuthenticatedRequest from "../../core/customInterfaces";
+import { validateRequiredFields } from "../../core/helpers/comun";
+import Todo from "./model";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc"; // Usar 'import' para el plugin de UTC
+import timezone from "dayjs/plugin/timezone";
+
+dayjs.extend(utc); // Extender dayjs con el plugin de UTC
+dayjs.extend(timezone);
 
 export const addTodo = async (
   req: AuthenticatedRequest,
@@ -11,18 +19,33 @@ export const addTodo = async (
     // Obtener datos del cuerpo de la solicitud
     const { tituloTarea, descripcion, tiempo, tiempoFin, estado, prioridad } =
       req.body;
-    console.log(tituloTarea, descripcion, tiempo, tiempoFin, estado, prioridad);
 
-    // Busca al usuario por ID
-    // const user = await User.findById(userId);
+    const camposRequeridos = [
+      "tituloTarea",
+      "descripcion",
+      "tiempo",
+      "tiempoFin",
+      "estado",
+      "prioridad",
+    ];
 
-    // if (!user) {
-    //   const error = new Error("Usuario no encontrado");
-    //   (error as any).status = 404;
-    //   throw error;
-    // }
+    // Verificar campos requeridos utilizando la función de validación
+    validateRequiredFields(req.body, camposRequeridos);
 
-    // res.status(200).json(user);
+    // Crear una nueva tarea con el usuario autenticado
+    const newTodo = new Todo({
+      usuario: userId,
+      tarea: tituloTarea,
+      descripcion,
+      fechaInicio: dayjs(tiempo).tz("Europe/Madrid").format(), // Usar las fechas convertidas a UTC
+      fechaFin: dayjs(tiempoFin).tz("Europe/Madrid").format(), // Usar las fechas convertidas a UTC
+      estado,
+      prioridad,
+    });
+
+    await newTodo.save();
+
+    res.status(201).json({ message: "Se ha agregado una nueva tarea" });
   } catch (error) {
     next(error);
   }
