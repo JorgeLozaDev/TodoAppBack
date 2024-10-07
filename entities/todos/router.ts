c0ee0967325d4feaee60b2883686c6b938ec8423
jorge <j0rg3e93@gmail.com>
2024-10-08 00:03:36 +0200

    ADD: Se agrega el endpoint para obtener los todos del usuario

--- entities/todos/router.ts
+++ entities/todos/router.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { authMiddleware } from "../../middlewares/authMIddleware";
-import { addTodo } from "./controller";
+import { addTodo, getAllTodosUser } from "./controller";
 
 const router = express.Router();
 
 router.post("/addTodo", authMiddleware, addTodo);
+router.get("/listTodos", authMiddleware, getAllTodosUser);
 
 export default router;
