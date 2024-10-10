cc50f4866eeca0022b105f9842c00dc48234a4e8
jorge <j0rg3e93@gmail.com>
2024-10-11 00:15:04 +0200

    UPDATE: Se agrega el endpoint para el borrado de tareas

--- entities/todos/router.ts
+++ entities/todos/router.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { authMiddleware } from "../../middlewares/authMIddleware";
-import { addTodo, getAllTodosUser } from "./controller";
+import { addTodo, deleteTodos, getAllTodosUser } from "./controller";
 
 const router = express.Router();
 
 router.post("/addTodo", authMiddleware, addTodo);
 router.get("/listTodos", authMiddleware, getAllTodosUser);
+router.delete("/deleteTodos", authMiddleware, deleteTodos);
 
 export default router;
