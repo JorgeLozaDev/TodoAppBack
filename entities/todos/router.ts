3f5edeb90575e5077eed9adb763e267ff00167b9
jorge <j0rg3e93@gmail.com>
2024-10-15 00:05:20 +0200

    UPDATE: Se agrega el endpoint para sacar la información de un todo

--- entities/todos/router.ts
+++ entities/todos/router.ts
@@ -1,11 +1,18 @@
 import express from "express";
 import { authMiddleware } from "../../middlewares/authMIddleware";
-import { addTodo, deleteTodos, getAllTodosUser } from "./controller";
+import {
+  addTodo,
+  deleteTodos,
+  getAllTodosUser,
+  todoDetails,
+} from "./controller";
 
 const router = express.Router();
 
 router.post("/addTodo", authMiddleware, addTodo);
 router.get("/listTodos", authMiddleware, getAllTodosUser);
 router.delete("/deleteTodos", authMiddleware, deleteTodos);
+
+router.get("/todo/:id", authMiddleware, todoDetails);
 
 export default router;
